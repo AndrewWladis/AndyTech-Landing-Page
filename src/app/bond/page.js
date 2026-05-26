e9bc630f76b68d328f3acdb222f78107e8bc19bf
Andrew Wladis <andrewwladis@gmail.com>
2026-05-26 09:44:59 -0400

    Fixed bonds ai link

--- src/app/bond/page.js
+++ src/app/bond/page.js
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation";
 
-const BONDS_SAI_URL = "https://apps.apple.com/us/app/bonds-ai/id6767971558";
+const BONDS_SAI_URL = "https://apps.apple.com/us/app/bondgs-ai/id6767971558";
 
 export default function BondsaiPage() {
   redirect(BONDS_SAI_URL);
